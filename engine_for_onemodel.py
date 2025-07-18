685226b021835125d675709320b4fd8552e4a353
saddy-paddy <109652308+saddy-paddy@users.noreply.github.com>
2025-07-18 22:47:45 +0530

    Update engine_for_onemodel.py

--- engine_for_onemodel.py
+++ engine_for_onemodel.py
@@ -38,148 +38,7 @@ def get_loss_scale_for_deepspeed(model):
     optimizer = model.optimizer
     return optimizer.loss_scale if hasattr(optimizer, "loss_scale") else optimizer.cur_scale
 
-'''
-def train_one_epoch(args, model: torch.nn.Module, criterion: torch.nn.Module,criterion2,
-                    data_loader: Iterable, optimizer: torch.optim.Optimizer,
-                    device: torch.device, epoch: int, loss_scaler, max_norm: float = 0,
-                    model_ema: Optional[ModelEma] = None, mixup_fn: Optional[Mixup] = None, log_writer=None,
-                    start_steps=None, lr_schedule_values=None, wd_schedule_values=None,
-                    num_training_steps_per_epoch=None, update_freq=None):
-    
-    model.train(True)
-    metric_logger = utils.MetricLogger(delimiter="  ")
-    print("You are at training")
-    metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value:.6f}'))
-    metric_logger.add_meter('min_lr', utils.SmoothedValue(window_size=1, fmt='{value:.6f}'))
-    header = 'Epoch: [{}]'.format(epoch)
-    print_freq = 5
 
-    if loss_scaler is None:
-        model.zero_grad()
-        model.micro_steps = 0
-    else:
-        optimizer.zero_grad()
-
-    for data_iter_step, (samples, targets, _, _) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
-        print('data_iter_step',data_iter_step)
-        step = data_iter_step // update_freq
-        if step >= num_training_steps_per_epoch:
-            continue
-        it = start_steps + step  # global training iteration
-        # Update LR & WD for the first acc
-        if lr_schedule_values is not None or wd_schedule_values is not None and data_iter_step % update_freq == 0:
-            for i, param_group in enumerate(optimizer.param_groups):
-                if lr_schedule_values is not None:
-                    param_group["lr"] = lr_schedule_values[it] * param_group["lr_scale"]
-                if wd_schedule_values is not None and param_group["weight_decay"] > 0:
-                    param_group["weight_decay"] = wd_schedule_values[it]
-
-        samples = samples.to(device, non_blocking=True)
-
-        print(targets,"targets-----------")
-        targets = targets.to(device, non_blocking=True)
-
-        if mixup_fn is not None:
-            print(f"Actual batch size received by mixup_fn: {len(samples)}")
-            samples, targets = mixup_fn(samples, targets)
-        #print("target after mixup", targets)
-        if loss_scaler is None:
-            samples = samples.half()
-            loss, output = cross_train_class_batch(
-                model, samples, targets, criterion,criterion2)
-        else:
-            with torch.cuda.amp.autocast():
-                samples = samples.half()
-                loss, output = cross_train_class_batch(
-                    model, samples, targets, criterion,criterion2)
-                print("loss",loss)
-        loss_value = loss.item()    
-
-        if not math.isfinite(loss_value):
-            print("Loss is {}, stopping training".format(loss_value))
-            sys.exit(1)
-
-        if loss_scaler is None:
-            loss = loss/update_freq
-            model.backward(loss)
-            model.step()
-            
-
-            if (data_iter_step + 1) % update_freq == 0:
-                # model.zero_grad()
-                # Deepspeed will call step() & model.zero_grad() automatic
-                if model_ema is not None:
-                    model_ema.update(model)
-            grad_norm = None
-            loss_scale_value = get_loss_scale_for_deepspeed(model)
-        else:
-            # this attribute is added by timm on one optimizer (adahessian)
-            is_second_order = hasattr(optimizer, 'is_second_order') and optimizer.is_second_order
-            #loss /= update_freq
-            loss=loss/update_freq
-            print("loss ok",loss)
-            if torch.isnan(loss):
-                print("Loss is nan")
-                exit()
-            
-            
-            grad_norm = loss_scaler(loss, optimizer, clip_grad=max_norm,
-                                    parameters=model.parameters(), create_graph=is_second_order,
-                                    update_grad=(data_iter_step + 1) % update_freq == 0)
-                                    
-            
-            
-                                    
-            print("all ok part 2")
-            if (data_iter_step + 1) % update_freq == 0:
-                optimizer.zero_grad()
-                if model_ema is not None:
-                    ("This runs")
-                    model_ema.update(model)
-            loss_scale_value = loss_scaler.state_dict()["scale"]
-
-        torch.cuda.synchronize()
-
-        if mixup_fn is None:
-            class_acc = (output.max(-1)[-1] == targets).float().mean()
-        else:
-            class_acc = None
-        metric_logger.update(loss=loss_value)
-        metric_logger.update(class_acc=class_acc)
-        metric_logger.update(loss_scale=loss_scale_value)
-        min_lr = 10.
-        max_lr = 0.
-        for group in optimizer.param_groups:
-            min_lr = min(min_lr, group["lr"])
-            max_lr = max(max_lr, group["lr"])
-
-        metric_logger.update(lr=max_lr)
-        metric_logger.update(min_lr=min_lr)
-        weight_decay_value = None
-        for group in optimizer.param_groups:
-            if group["weight_decay"] > 0:
-                weight_decay_value = group["weight_decay"]
-        metric_logger.update(weight_decay=weight_decay_value)
-        metric_logger.update(grad_norm=grad_norm)
-
-        if log_writer is not None:
-            log_writer.update(loss=loss_value, head="loss")
-            log_writer.update(class_acc=class_acc, head="loss")
-            log_writer.update(loss_scale=loss_scale_value, head="opt")
-            log_writer.update(lr=max_lr, head="opt")
-            log_writer.update(min_lr=min_lr, head="opt")
-            log_writer.update(weight_decay=weight_decay_value, head="opt")
-            log_writer.update(grad_norm=grad_norm, head="opt")
-
-            log_writer.set_step()
-
-    # gather the stats from all processes
-    metric_logger.synchronize_between_processes()
-    print("Averaged stats:", metric_logger)
-    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
-
-
-'''
 def train_one_epoch(args, model: torch.nn.Module, criterion: torch.nn.Module, criterion2,
                     data_loader: Iterable, optimizer: torch.optim.Optimizer,
                     device: torch.device, epoch: int, loss_scaler, max_norm: float = 0,
@@ -316,124 +175,7 @@ def train_one_epoch(args, model: torch.nn.Module, criterion: torch.nn.Module, cr
     return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
 
 
-'''
-def train_one_epoch(args, model: torch.nn.Module, criterion: torch.nn.Module, criterion2,
-                    data_loader: Iterable, optimizer: torch.optim.Optimizer,
-                    device: torch.device, epoch: int, loss_scaler, max_norm: float = 0,
-                    model_ema: Optional[ModelEma] = None, mixup_fn: Optional[Mixup] = None, log_writer=None,
-                    start_steps=None, lr_schedule_values=None, wd_schedule_values=None,
-                    num_training_steps_per_epoch=None, update_freq=None, buffer=None):
 
-    model.train(True)
-    metric_logger = utils.MetricLogger(delimiter="  ")
-    print("Training with Disk-Based DER (8 samples per iteration)")
-    metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value:.6f}'))
-    metric_logger.add_meter('min_lr', utils.SmoothedValue(window_size=1, fmt='{value:.6f}'))
-    header = f'Epoch: [{epoch}]'
-    print_freq = 1
-
-    if loss_scaler is None:
-        model.zero_grad()
-        model.micro_steps = 0
-    else:
-        optimizer.zero_grad()
-
-    for data_iter_step, (samples, targets, _, _) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
-        print(f'data_iter_step {data_iter_step}')
-
-        step = data_iter_step // update_freq
-        if step >= num_training_steps_per_epoch:
-            continue
-
-        it = start_steps + step  # Global training iteration
-
-        if lr_schedule_values is not None or wd_schedule_values is not None and data_iter_step % update_freq == 0:
-            for i, param_group in enumerate(optimizer.param_groups):
-                if lr_schedule_values is not None:
-                    param_group["lr"] = lr_schedule_values[it] * param_group["lr_scale"]
-                if wd_schedule_values is not None and param_group["weight_decay"] > 0:
-                    param_group["weight_decay"] = wd_schedule_values[it]
-
-        # Move data to GPU
-        samples = samples.to(device, non_blocking=True)
-        targets = targets.to(device, non_blocking=True)
-
-        if mixup_fn is not None:
-            print(f"Actual batch size received by mixup_fn: {len(samples)}")
-            samples, targets = mixup_fn(samples, targets)
-            if torch.isnan(targets).any():  # ✅ Check if Mixup causes NaN
-                print("Warning: NaN detected in targets after Mixup, skipping batch.")
-                continue
-
-        # 🔹 Load only 8 samples & features from the oldest stored batch
-        if data_iter_step>1:
-            buf_samples, buf_features = buffer.load_data(device=device)
-            print("buff_samples:", buff_samples, buff_features, end="/n")
-            
-        else:
-            print("none---------------------------------------------none")
-            buf_samples,buf_features= None, None
-
-        # 🔹 Compute loss using both fresh & replayed samples
-        if loss_scaler is None:
-            samples = samples.half()
-            loss, output, features = cross_train_class_batch(
-                model, samples, targets, criterion, criterion2, buf_samples, buf_features
-            )
-        else:
-            with torch.cuda.amp.autocast():
-                samples = samples.half()
-                loss, output, features = cross_train_class_batch(
-                    model, samples, targets, criterion, criterion2, buf_samples, buf_features
-                )
-        loss_value = loss.item()
-
-        if not math.isfinite(loss_value):
-            print(f"Loss is {loss_value}, stopping training")
-            sys.exit(1)
-
-        # Backpropagation
-        if loss_scaler is None:
-            loss = loss / update_freq
-            model.backward(loss)
-            model.step()
-
-            if (data_iter_step + 1) % update_freq == 0:
-                if model_ema is not None:
-                    model_ema.update(model)
-            grad_norm = None
-            loss_scale_value = get_loss_scale_for_deepspeed(model)
-        else:
-            is_second_order = hasattr(optimizer, 'is_second_order') and optimizer.is_second_order
-            loss = loss / update_freq
-            if torch.isnan(loss):
-                print("Loss is NaN, exiting...")
-                exit()
-
-            grad_norm = loss_scaler(loss, optimizer, clip_grad=max_norm,
-                                    parameters=model.parameters(), create_graph=is_second_order,
-                                    update_grad=(data_iter_step + 1) % update_freq == 0)
-
-            if (data_iter_step + 1) % update_freq == 0:
-                torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)  # ✅ Prevents large gradient updates
-
-                optimizer.zero_grad()
-                if model_ema is not None:
-                    model_ema.update(model)
-            loss_scale_value = loss_scaler.state_dict()["scale"]
-
-        torch.cuda.synchronize()
-
-        # 🔹 Save current batch to disk for replay
-        buffer.save_data(data_iter_step, samples, features.data)
-
-        metric_logger.update(loss=loss_value)
-
-    # Gather stats from all processes
-    metric_logger.synchronize_between_processes()
-    print("Averaged stats:", metric_logger)
-    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
-'''
 @torch.no_grad()
 def validation_one_epoch(args, data_loader, model, device):
     criterion=BatchAllTtripletLoss()
@@ -473,7 +215,7 @@ def validation_one_epoch(args, data_loader, model, device):
 
     return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
 
-'''
+
 def predict_with_topk_experts_batch(selector, experts, videos, device, vote='maxlogit'):
     with torch.no_grad():
         B = videos.size(0)
@@ -511,8 +253,8 @@ def predict_with_topk_experts_batch(selector, experts, videos, device, vote='max
             final_logits.append(final_logit.unsqueeze(0))
 
         return torch.stack(final_preds), torch.cat(final_logits, dim=0)
-'''
-def predict_with_topk_experts_batch(selector, experts, videos, device, vote='maxlogit'):
+
+def predict_with_weighted_topk_experts_batch(selector, experts, videos, device, vote='maxlogit'):
     with torch.no_grad():
         B = videos.size(0)
         selector_output = selector(videos.to(device))
@@ -612,10 +354,10 @@ def final_test(args, data_loader, selector, experts, device, file):
     return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
         
 
-'''
+
 
 @torch.no_grad()
-def final_test(args, data_loader, model, device, file):
+def final_test_individual(args, data_loader, model, device, file):
     criterion = torch.nn.CrossEntropyLoss()
 
     metric_logger = utils.MetricLogger(delimiter="  ")
@@ -695,9 +437,7 @@ def final_test(args, data_loader, model, device, file):
         metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
         metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
     
-    #print(f"Shapes: Output: {len(Out)}, labels:{len(labels)}, features: {features.shape}, {type(features)}") 
-    #print("labels",labels)
-    #print("Out", Out, len(Out))
+   
     
     import pandas as pd
     
@@ -821,7 +561,6 @@ def final_test(args, data_loader, model, device, file):
     print("Return statement")
     return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
 
-'''
 
 def merge(eval_path, num_tasks):
     dict_feats = {}
